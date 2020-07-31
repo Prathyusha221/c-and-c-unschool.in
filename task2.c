11511642a0ba4550341071688a23c5dc0ac7461d
Prathyusha221 <65334227+Prathyusha221@users.noreply.github.com>
2020-07-31 20:03:57 +0530

    Add files via upload

---
+++ task2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b,c;
+    printf("enter three numbers: \n");
+    scanf("%d %d %d", &a,&b,&c);
+    printf("Sum of %d+%d+%d is %d", a, b, c, a+b+c);
+    return 0;
+}
